989c45159a35ff3468b6f6d32c1041c839f27d11
Hwa_Baeg_ <aoadid8881@naver.com>
2020-09-16 12:51:27 +0900

    Update EventListener.php

--- ServerInformation/src/ServerInformation/EventListener.php
+++ ServerInformation/src/ServerInformation/EventListener.php
@@ -10,34 +10,36 @@ use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\block\BlockBreakEvent;
 
 use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
 use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\utils\TextFormat;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityDeathEvent;
 use pocketmine\item\Item;
 use pocketmine\tile\Chest;
 use ServerInformation\Inventory\DoubleChestInventory;
 use pocketmine\inventory\transaction\action\SlotChangeAction;
 use pocketmine\event\inventory\InventoryCloseEvent;
-use pocketmine\inventory\ContainerInventory;
 
 class EventListener implements Listener
 {
-  
+
   protected $plugin;
-  
+
   public function __construct(ServerInformation $plugin)
   {
     $this->plugin = $plugin;
   }
-  public function onInvClose(InventoryCloseEvent $ev) {
-    $player = $ev->getPlayer();
-    $inv = $ev->getInventory();
+  public function onJoin(PlayerJoinEvent $event) {
+    $player = $event->getPlayer();
+    $name = $player->getName();
+    if (!isset($this->plugin->pldb [$name])){
+      $this->plugin->pldb [$name] ["Page"] = 1;
+      $this->plugin->save ();
+    }
+  }
+  public function onInvClose(InventoryCloseEvent $event) {
+    $player = $event->getPlayer();
+    $inv = $event->getInventory();
     if ($inv instanceof DoubleChestInventory) {
       $inv->onClose($player);
       return true;
@@ -60,6 +62,45 @@ class EventListener implements Listener
               $inv->onClose ($player);
               $this->plugin->onPlayerOpen ($player);
               return true;
+            }
+            if ($inv->getItem ($slot)->getCustomName() == "§r§f다음 페이지"){
+              $this->plugin->pldb [$name] ["Page"] += 1;
+              $this->plugin->save ();
+              $page = (int)$this->plugin->pldb [$name] ["Page"];
+              if (isset($this->plugin->pldb ["플레이어"] [$page])){
+                $event->setCancelled ();
+                $inv->onClose ($player);
+                $this->plugin->onPlayerOpen ($player);
+                return true;
+              } else {
+                $player->sendMessage ("[ 서버정보 ] 이전 페이지는 존재하지 않습니다.");
+                $event->setCancelled ();
+                $inv->onClose ($player);
+                return true;
+              }
+            }
+            if ($inv->getItem ($slot)->getCustomName() == "§r§f이전 페이지"){
+              $this->plugin->pldb [$name] ["Page"] -= 1;
+              $this->plugin->save ();
+              $page = (int)$this->plugin->pldb [$name] ["Page"];
+              if ($page >= 1){
+                if (isset($this->plugin->pldb ["플레이어"] [$page])){
+                  $event->setCancelled ();
+                  $inv->onClose ($player);
+                  $this->plugin->onPlayerOpen ($player);
+                  return true;
+                } else {
+                  $player->sendMessage ("[ 서버정보 ] 이전 페이지는 존재하지 않습니다.");
+                  $event->setCancelled ();
+                  $inv->onClose ($player);
+                  return true;
+                }
+              } else {
+                $player->sendMessage ("[ 서버정보 ] 이전 페이지는 존재하지 않습니다.");
+                $event->setCancelled ();
+                $inv->onClose ($player);
+                return true;
+              }
             }
             if ($inv->getItem ($slot)->getCustomName() == "§r§f서버컨탠츠"){
               $event->setCancelled ();
