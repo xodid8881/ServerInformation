fef23f8a6709498a1d1f2c80395d3921260592ea
Hwa_Baeg_ <aoadid8881@naver.com>
2023-01-10 14:54:52 +0900

    Update ServerInformation.php

--- ServerInformation/src/ServerInformation/ServerInformation.php
+++ ServerInformation/src/ServerInformation/ServerInformation.php
@@ -3,13 +3,14 @@ declare(strict_types=1);
 
 namespace ServerInformation;
 
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 use pocketmine\network\mcpe\protocol\OnScreenTextureAnimationPacket;
 use pocketmine\utils\Config;
 use pocketmine\scheduler\Task;
+use pocketmine\item\Item;
 use ServerInformation\Commands\EventCommand;
 
 use ServerInformation\InventoryLib\InvLibManager;
@@ -22,11 +23,10 @@ use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\block\Block;
 use pocketmine\tile\Chest;
 // monster
-
-use pocketmine\world\World;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIds;
+use pocketmine\world\World;
 
 class ServerInformation extends PluginBase
 {
@@ -40,12 +40,12 @@ class ServerInformation extends PluginBase
     return static::$instance;
   }
   
-  public function onLoad()
+  public function onLoad():void
   {
     self::$instance = $this;
   }
   
-  public function onEnable()
+  public function onEnable():void
   {
     $this->player = new Config ($this->getDataFolder() . "players.yml", Config::YAML);
     $this->pldb = $this->player->getAll();
@@ -59,19 +59,20 @@ class ServerInformation extends PluginBase
     $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
     $this->getScheduler ()->scheduleRepeatingTask ( new PlayerSaveTask ( $this, $this->player ), 20*10 );
   }
-  public function onConTentsUIOpen ($player) {
+  public function onConTentsUIOpen ($player):void
+  {
     $this->getScheduler()->scheduleDelayedTask(new class ($this, $player) extends Task {
       protected $owner;
       public function __construct(ServerInformation $owner,Player $player) {
         $this->owner = $owner;
         $this->player = $player;
       }
-      public function onRun($currentTick) {
+      public function onRun():void {
         $this->owner->ConTentsUI($this->player);
       }
     }, 20);
   }
-  public function ConTentsUI(Player $player)
+  public function ConTentsUI(Player $player):void
   {
     $encode = [
       'type' => 'form',
@@ -86,8 +87,7 @@ class ServerInformation extends PluginBase
     $packet = new ModalFormRequestPacket ();
     $packet->formId = 156321;
     $packet->formData = json_encode($encode);
-    $player->sendDataPacket($packet);
-    return true;
+    $player->getNetworkSession()->sendDataPacket($packet);
   }
   public function onClausesOpen ($player) {
     $this->getScheduler()->scheduleDelayedTask(new class ($this, $player) extends Task {
@@ -96,12 +96,12 @@ class ServerInformation extends PluginBase
         $this->owner = $owner;
         $this->player = $player;
       }
-      public function onRun($currentTick) {
+      public function onRun():void {
         $this->owner->ClausesUI($this->player);
       }
     }, 20);
   }
-  public function ClausesUI(Player $player)
+  public function ClausesUI(Player $player):void
   {
     $encode = [
       'type' => 'form',
@@ -116,10 +116,10 @@ class ServerInformation extends PluginBase
     $packet = new ModalFormRequestPacket ();
     $packet->formId = 156322;
     $packet->formData = json_encode($encode);
-    $player->sendDataPacket($packet);
-    return true;
+    $player->getNetworkSession()->sendDataPacket($packet);
   }
-  public function onPlayerInfoOpen ($player,$players) {
+  public function onPlayerInfoOpen ($player,$players):void
+  {
     $this->getScheduler()->scheduleDelayedTask(new class ($this, $player, $players) extends Task {
       protected $owner;
       public function __construct(ServerInformation $owner,Player $player,Player $players) {
@@ -127,12 +127,12 @@ class ServerInformation extends PluginBase
         $this->player = $player;
         $this->players = $players;
       }
-      public function onRun($currentTick) {
+      public function onRun():void {
         $this->owner->PlayerInfo($this->player,$this->players);
       }
     }, 20);
   }
-  public function PlayerInfo(Player $player, $players)
+  public function PlayerInfo(Player $player, $players):void
   {
     $playername = $players->getName ();
     $encode = [
@@ -148,50 +148,55 @@ class ServerInformation extends PluginBase
     $packet = new ModalFormRequestPacket ();
     $packet->formId = 156322;
     $packet->formData = json_encode($encode);
-    $player->sendDataPacket($packet);
-    return true;
+    $player->getNetworkSession()->sendDataPacket($packet);
   }
-  public function onPlayerOpen($player) {
+  public function onPlayerOpen($player):void
+  {
     $name = $player->getName ();
-    $inv = new DoubleChestInventory("§6§l[ §f서버 동접자 §6]");
+    $inv = InvLibManager::create(LibInvType::DOUBLE_CHEST(), new Position($playerPos->x, $playerPos->y - 2, $playerPos->z, $playerPos->getWorld()), '§6§l[ §f서버 동접자 §6]');
     $page = (int)$this->pldb [$name] ["Page"];
     if (isset($this->pldb ["플레이어"] [$page])) {
       foreach($this->pldb ["플레이어"] [$page] as $count => $v){
         $name = $this->pldb ["플레이어"] [$page] [$count];
-        $CheckItem = Item::get(397, 3, 1)->setCustomName("{$name}")->setLore([ "§r§7해당 플레이어의 정보를 봅니다.\n인벤토리로 가져가보세요." ]);
+        $CheckItem = ItemFactory::getInstance()->get(397, 3, 1)->setCustomName("{$name}")->setLore([ "§r§7해당 플레이어의 정보를 봅니다.\n인벤토리로 가져가보세요." ]);
         $inv->setItem( $count , $CheckItem );
-        $inv->setItem( 53 , Item::get(144, 0, 1)->setCustomName("§r§f다음 페이지")->setLore([ "§r§7다음 페이지로 이동합니다.\n인벤토리로 가져가보세요." ]) );
-        $inv->setItem( 45 , Item::get(144, 0, 1)->setCustomName("§r§f이전 페이지")->setLore([ "§r§7이전 페이지로 이동합니다.\n인벤토리로 가져가보세요." ]) );
+        $inv->setItem( 53 , ItemFactory::getInstance()->get(144, 0, 1)->setCustomName("§r§f다음 페이지")->setLore([ "§r§7다음 페이지로 이동합니다.\n인벤토리로 가져가보세요." ]) );
+        $inv->setItem( 45 , ItemFactory::getInstance()->get(144, 0, 1)->setCustomName("§r§f이전 페이지")->setLore([ "§r§7이전 페이지로 이동합니다.\n인벤토리로 가져가보세요." ]) );
       }
     }
-    $inv->sendContents($inv->getViewers());
+    $inv->setListener(function(InvLibAction $action):void{
+      $action->setCancelled();
+    });
     $this->getScheduler()->scheduleDelayedTask(new class ($player, $inv) extends Task {
       public function __construct($player, $inv) {
         $this->player = $player;
         $this->inv = $inv;
       }
-      public function onRun($currentTick) {
-        $this->player->addWindow($this->inv);
+      public function onRun():void {
+        $this->inv->send($this->player);
       }
     }, 20);
   }
-  public function onOpen($player) {
+  public function onOpen($player):void
+  {
     $name = $player->getName ();
-    $inv = new DoubleChestInventory("§6§l[ §f서버정보 §6]");
-    $CheckItem = Item::get(144, 0, 1)->setCustomName("§r§f서버동접")->setLore([ "§r§7서버의 동시접속자들을 확인합니다.\n인벤토리로 가져가보세요." ]);
+    $inv = InvLibManager::create(LibInvType::DOUBLE_CHEST(), new Position($playerPos->x, $playerPos->y - 2, $playerPos->z, $playerPos->getWorld()), '§6§l[ §f서버정보 §6]');
+    $CheckItem = ItemFactory::getInstance()->get(144, 0, 1)->setCustomName("§r§f서버동접")->setLore([ "§r§7서버의 동시접속자들을 확인합니다.\n인벤토리로 가져가보세요." ]);
     $inv->setItem( 1 , $CheckItem );
-    $CheckItem = Item::get(144, 0, 1)->setCustomName("§r§f서버콘텐츠")->setLore([ "§r§7서버의 콘텐츠를 확인합니다.\n인벤토리로 가져가보세요." ]);
+    $CheckItem = ItemFactory::getInstance()->get(144, 0, 1)->setCustomName("§r§f서버콘텐츠")->setLore([ "§r§7서버의 콘텐츠를 확인합니다.\n인벤토리로 가져가보세요." ]);
     $inv->setItem( 4 , $CheckItem );
-    $CheckItem = Item::get(144, 0, 1)->setCustomName("§r§f서버약관")->setLore([ "§r§7서버의 약관을 확인합니다.\n인벤토리로 가져가보세요." ]);
+    $CheckItem = ItemFactory::getInstance()->get(144, 0, 1)->setCustomName("§r§f서버약관")->setLore([ "§r§7서버의 약관을 확인합니다.\n인벤토리로 가져가보세요." ]);
     $inv->setItem( 7 , $CheckItem );
-    $inv->sendContents($inv->getViewers());
+    $inv->setListener(function(InvLibAction $action):void{
+      $action->setCancelled();
+    });
     $this->getScheduler()->scheduleDelayedTask(new class ($player, $inv) extends Task {
       public function __construct($player, $inv) {
         $this->player = $player;
         $this->inv = $inv;
       }
-      public function onRun($currentTick) {
-        $this->player->addWindow($this->inv);
+      public function onRun():void {
+        $this->inv->send($this->player);
       }
     }, 20);
   }
@@ -205,11 +210,11 @@ class ServerInformation extends PluginBase
       }
     }
   }
-  public function onDisable()
+  public function onDisable():void
   {
     $this->save();
   }
-  public function save()
+  public function save():void
   {
     $this->player->setAll($this->pldb);
     $this->player->save();
@@ -226,7 +231,7 @@ class PlayerSaveTask extends Task {
     $this->player = $player;
     $this->pldb = $this->player->getAll ();
   }
-  public function onRun(int $currentTick) {
+  public function onRun():void {
     $count = 0;
     $page = 1;
     foreach ( $this->owner->getServer ()->getOnlinePlayers () as $player ) {
