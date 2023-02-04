311be31f3dfe0397a6fc4f73abc2ca42bbc70b61
Hwa_Baeg_ <aoadid8881@naver.com>
2023-02-05 02:52:37 +0900

    Update EventCommand.php

--- ServerInformation/src/ServerInformation/Commands/EventCommand.php
+++ ServerInformation/src/ServerInformation/Commands/EventCommand.php
@@ -13,6 +13,7 @@ class EventCommand extends Command
 {
 
   protected $plugin;
+  private $chat;
 
   public function __construct(ServerInformation $plugin)
   {
@@ -22,7 +23,19 @@ class EventCommand extends Command
 
   public function execute(CommandSender $sender, string $commandLabel, array $args)
   {
-    $this->plugin->onOpen ($sender);
-    return true;
+    $name = $sender->getName ();
+    if (! isset ( $this->chat [$name] )) {
+      $this->plugin->onOpen ($sender);
+      $this->chat [$name] = date("YmdHis",strtotime ("+3 seconds"));
+      return true;
+    }
+    if (date("YmdHis") - $this->chat [$name] < 3) {
+      $sender->sendMessage ( ServerInformation::PREFIX . "이용 쿨타임이 지나지 않아 불가능합니다." );
+      return true;
+    } else {
+      $this->plugin->onOpen ($sender);
+      $this->chat [$name] = date("YmdHis",strtotime ("+3 seconds"));
+      return true;
+    }
   }
 }
